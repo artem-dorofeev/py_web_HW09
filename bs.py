cbd5190eeb4601fc4531bf159f75a1178117ff10
Artem Dorofeev <130536391+artem-dorofeev@users.noreply.github.com>
2023-10-23 16:42:00 +0300

    add lesson

---
+++ bs.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+url = 'https://quotes.toscrape.com/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('span', class_='text')
+authors = soup.find_all('small', class_='author')
+tags = soup.find_all('div', class_='tags')
+
+data = {}
+for i in range(0, len(quotes)):
+    tag = []
+    print('+' * 10)
+    # print(quotes[i].text)
+    quote = quotes[i].text
+    # print('--' + authors[i].text)
+    author = authors[i].text
+    tagsforquote = tags[i].find_all('a', class_='tag')
+    for tagforquote in tagsforquote:
+        print(tagforquote.text)
+        tag.append(tagforquote.text)
+    data = {
+        "Author": author,
+        "quote": quote,
+        "tag": tag
+    }
+
+    with open("quotes1.json", "w", encoding="utf-8") as fd:
+        json.dump(data, fd, ensure_ascii=False)
+
+print(data)
